f5e99f8db54e51f9bdd95a4eafcad009cf69ffa0
sugam kuber <sugam7842692@gmail.com>
2022-02-24 23:34:16 +0530

    Add files via upload

---
+++ my final Portfolio/src/components/Contact.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaPinterest,
+  FaInstagram,
+  
+} from "react-icons/fa";
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="container">
+        <div className="contactSection">
+          <div className="row justifyConter">
+            <div className="col-6">
+              <div className="contactSection-logo">
+                <img src="/images/logo.png" alt="" />
+              </div>
+              <p>
+                THANK YOU, please feel free to contact me, through my social handels
+              </p>
+              <ul className="contactCircles">
+                <li>
+                  <FaFacebookF className="contactIcon" />
+                </li>
+                <li>
+                  <FaTwitter className="contactIcon" />
+                </li>
+                <li>
+                  <FaPinterest className="contactIcon" />
+                </li>
+                <li>
+                  <FaInstagram className="contactIcon" />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
